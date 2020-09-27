dd3c3467e9717690c6b18a058aa81bd5de9230ae
dusarah <71254323+dusarah@users.noreply.github.com>
2020-09-27 09:19:54 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,24 @@
+function weekdays(){
+  var day=document.getElementById("day").value;
+  var enterDate=day.split("-");
+  var dd=parseInt(enterDate[2]);
+    var mm=parseInt(enterDate[1]);
+    var yy=parseInt(enterDate[0]);
+    var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","yaa","Afua","Ama"];
+    var cc=(yy-1)/100+1;
+    var birthDay=(cc/4-2*cc-1+5*yy/4+26*(mm+1)/10+dd)%7;
+    document.getElementById("display").innerHTML=Math.round(birthDay);
+    var gender=document.querySelector('input[name="optradio"]:checked').value;;
+    console.log(gender);
+    if (gender=="male"){
+      console.log(maleNames[Math.round(birthDay)]);
+      document.getElementById("names").innerHTML=maleNames[Math.round(birthDay)];
+
+    }
+  else   if (gender=="female"){
+      console.log(femaleNames[Math.round(birthDay)]);
+      document.getElementById("names").innerHTML=femaleNames[Math.round(birthDay)];
+
+    }
+}
